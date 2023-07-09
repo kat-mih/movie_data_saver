e3afd5b06619f29bd5f929c7b94d93a2bcd15089
Kate S <kateshpudejko@gmail.com>
2023-07-09 09:42:11 +0200

    adding filtering and sorting

--- index.js
+++ index.js
@@ -1,30 +1,67 @@
 import * as cheerio from "cheerio";
 import axios from "axios";
 
-const vods = ["netflix", "hbo_max", "canal_plus", "disney"];
-const headers = ["Title", "VOD Title Service", "Rating"];
-
-vods.forEach((platform) => {
-  movieData(platform);
-});
-
 // gathering information about movies
-async function movieData(platform) {
-  const website = await axios.get(
-    `https://filmweb.pl/ranking/vod/${platform}/film/2023`
-  );
-  const $ = cheerio.load(website.data);
+async function fetchMovieData(platform) {
+  try {
+    const website = await axios.get(
+      `https://filmweb.pl/ranking/vod/${platform}/film/2023`
+    );
+    const $ = cheerio.load(website.data);
 
-  // get data about title, rating
-  const movies = new Map();
-  $(".rankingType__card")
-    .slice(0, 10)
-    .each((i, el) => {
-      const movieTitle = $(el).find(".rankingType__title").text();
-      const movieRating = $(el).find(".rankingType__rate--value").text();
-      movies.set(movieTitle, [movieTitle, platform, movieRating]);
+    // get data about title, rating
+    const movies = [];
+    $(".rankingType__card")
+      .slice(0, 10)
+      .each((i, el) => {
+        const movieTitle = $(el).find(".rankingType__title").text();
+        const movieRating = $(el)
+          .find(".rankingType__rate--value")
+          .text()
+          .replace(",", ".");
+        // fullfill an array with the objects
+        movies.push({
+          title: movieTitle,
+          vod: platform,
+          rating: parseFloat(movieRating),
+        });
+      });
+
+    return movies;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+// sort and filtering gathered object with the movie data
+async function scrapeMovies() {
+  try {
+    // list of VODs platforms
+    const vods = ["netflix", "hbo_max", "canal_plus", "disney"];
+    const headers = ["Title", "VOD Title Service", "Rating"];
+
+    const promises = vods.map((platform) => fetchMovieData(platform));
+    const results = await Promise.all(promises);
+    const scrappedData = results.flat();
+
+    // sorting an movies by rating (or title)
+    scrappedData.sort((movie1, movie2) => {
+      return (
+        movie2.rating - movie1.rating ||
+        movie1.title.localeCompare(movie2.title)
+      );
     });
 
-  console.log(movies);
-  // get csv
+    // unique movie titles
+    const result = new Map();
+    scrappedData.forEach((movie) =>
+      result.set(movie.title, Object.values(movie))
+    );
+
+    console.log(result);
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
+
+scrapeMovies();
